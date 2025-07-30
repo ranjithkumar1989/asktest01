270d5419b5c23f4be17e22b028a9b17846bf0cbc
ranjithkumar1989 <ranjithdevops2019@yahoo.com>
2025-07-30 20:49:17 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 apt-get update && \
 apt-get install -y python3 python3-pip && \
-pip3 install requests --root-user-action=ignore && \
-python3 /scripts/api.py >> /tmp/out.txt&&
+pip3 install requests && \
+python3 /scripts/api.py >> /tmp/out.txt &&
 sleep 3600
